9259da05e001a9f2b27eb3ee2a6e223cfa393717
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2022-09-05 11:24:05 -0300

    useState de Produtos

--- pf0670/cp1/src/App.jsx
+++ pf0670/cp1/src/App.jsx
@@ -1,9 +1,84 @@
 import React from "react"
+import { useState } from "react"
 
 export default function App() {
+
+    // const listaProdutos = [
+    //     {
+    //         nomeProduto: "Mouse Óptico",
+    //         qtd: 100,
+    //         categoria: "informatica"
+    //     }
+    //     ,
+    //     {
+    //         nomeProduto: "Teclado",
+    //         qtd: 500,
+    //         categoria: "informatica"
+    //     }
+    //     ,
+    //     {
+    //         nomeProduto: "Microfone Digital",
+    //         qtd: 2000,
+    //         categoria: "informatica"
+    //     }
+    //     ,
+    //     {
+    //         nomeProduto: "Lâmpada Led 15w",
+    //         qtd: 250,
+    //         categoria: "eletrica"
+    //     }
+    //     ,
+    //     {
+    //         nomeProduto: "Lenço de Papel",
+    //         qtd: 10000,
+    //         categoria: "higinene"
+    //     }
+    // ]
+
+    // const [produtos, setProdutos] = useState(listaProdutos)
+    const [produtos, setProdutos] = useState([
+        {
+            nomeProduto: "Mouse Óptico",
+            qtd: 100,
+            categoria: "informatica"
+        }
+        ,
+        {
+            nomeProduto: "Teclado",
+            qtd: 500,
+            categoria: "informatica"
+        }
+        ,
+        {
+            nomeProduto: "Microfone Digital",
+            qtd: 2000,
+            categoria: "informatica"
+        }
+        ,
+        {
+            nomeProduto: "Lâmpada Led 15w",
+            qtd: 250,
+            categoria: "eletrica"
+        }
+        ,
+        {
+            nomeProduto: "Lenço de Papel",
+            qtd: 10000,
+            categoria: "higinene"
+        }
+    ])
     return (
-        <div>
-            <h1>APP</h1>
-        </div>
+        <>
+            <h1>CP1 de RWD 2º SEMESTRE</h1>
+        </>
     )
 }
+
+// 2 – No App.jsx crie a seguinte estrutura. ( 3,5 Pontos)
+// a) Adicione um fragmento.
+// b) Um elemento h1 com o texto “CP1 de RWD 1º SEMESTRE”.
+// c) Declare um useState de nome produtos que recebe uma lista de produtos com no
+// mínimo 5 produtos e com os seguintes atributos:
+// nomeProduto = “Mouse Óptico”
+// qtd = 100
+// categoria = “informatica”, ...
