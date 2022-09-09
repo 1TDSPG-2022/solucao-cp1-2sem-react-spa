3af9d96687e9e064ecb165683d7ef66a282976cb
GuilherMenezesdaSilva <rm89384@fiap.com.br>
2022-09-09 09:54:23 -0300

    Introdução a sass

---
+++ RM89384/cp1/src/App.jsx
@@ -0,0 +1,48 @@
+import React from "react"
+import { useState } from "react"
+import Produto from "./Components/Produto"
+
+export default function App() {
+
+    const [produtos, setProdutos] = useState([
+        {
+            nomeProduto: "Mouse Óptico",
+            qtd: 100,
+            categoria: "informatica"
+        }
+        ,
+        {
+            nomeProduto: "Teclado",
+            qtd: 500,
+            categoria: "informatica"
+        }
+        ,
+        {
+            nomeProduto: "Microfone Digital",
+            qtd: 2000,
+            categoria: "informatica"
+        }
+        ,
+        {
+            nomeProduto: "Lâmpada Led 15w",
+            qtd: 250,
+            categoria: "eletrica"
+        }
+        ,
+        {
+            nomeProduto: "Lenço de Papel",
+            qtd: 10000,
+            categoria: "higinene"
+        }
+    ])
+    return (
+        <>
+            <h1>CP1 de RWD 2º SEMESTRE</h1>
+            <div class="container">
+                Escopo da div
+                <p>Escopo do parágrafo</p>
+            </div>
+            <Produto produtos={produtos} />
+        </>
+    )
+}
---
+++ RM89384/cp1/src/Components/Produto.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import "./Produto.scss"
+
+export default function Produto(props) {
+
+    return (
+        <div>
+            <h2>COMPONENTE PRODUTOS</h2>
+            <table className="tblProdutos">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Produto</th>
+                        <th>Quantidade</th>
+                        <th>Categoria</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {props.produtos.map((p, i) =>
+                        // <tr key={i} style={i % 2 === 0 ? { backgroundColor: "#ccc" } : { backgroundColor: "#ddd333" }} >
+                        <tr key={i}>
+                            <td>{i + 1}</td>
+                            <td>{p.nomeProduto}</td>
+                            <td>{p.qtd}</td>
+                            <td>{p.categoria}</td>
+                        </tr>
+                    )}
+                </tbody>
+                <tfoot>
+                    <tr>
+                        <td colSpan={4}>
+                            <caption>PRODUTOS</caption>
+                        </td>
+                    </tr>
+                </tfoot>
+            </table>
+        </div >
+    )
+}
+
+
+// 4 – No componente Produtos deve ser realizada através dos método necessários o consumo
+// da lista de produtos: (3, 5 Pontos)
+// a) A lista de produtos deve ser apresentada em uma < table >.
+//     b) Adicione a coluna ID a tabela.
+//         c) As linhas da tabela devem ser de cores diferentes, ou seja, uma tabela zebrada.
+// Utilize algum tipo de índice para poder realizar esse processo
