6633ffe3c5ee7344eddaaa1bfda0fe15ccf8984c
VitorRT <barberino.italia@gmail.com>
2022-09-09 09:56:19 -0300

    sass no react

---
+++ rm95209/cp1/src/App.jsx
@@ -0,0 +1,49 @@
+import React from "react"
+import { useState } from "react"
+import Produtos from "./components/Produto"
+
+export default function App() {
+
+    const [produtos, setProdutos] = useState([
+        {
+            nomeProduto: "Mouse Óptico",
+            qtd: 100,
+            categoria: "informatica"
+        }
+        ,
+        {
+            nomeProduto: "Teclado",
+            qtd: 500,
+            categoria: "informatica"
+        }
+        ,
+        {
+            nomeProduto: "Microfone Digital",
+            qtd: 2000,
+            categoria: "informatica"
+        }
+        ,
+        {
+            nomeProduto: "Lâmpada Led 15w",
+            qtd: 250,
+            categoria: "eletrica"
+        }
+        ,
+        {
+            nomeProduto: "Lenço de Papel",
+            qtd: 10000,
+            categoria: "higinene"
+        }
+    ])
+    return (
+        <>
+            <h1>CP1 de RWD 2º SEMESTRE</h1>
+            <div>
+                Escopo da Div
+                <p>Escopo do parágrafo</p>
+            </div>
+            <Produtos produtos={produtos} />
+        </>
+    )
+}
+
---
+++ rm95209/cp1/src/components/Produto.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import "./produto.scss"
+
+export default function Produtos(props) {
+
+    return (
+        <div>
+            <h2>COMPONENTE PRODUTOS</h2>
+            <table className="tblProdutos">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Produto</th>
+                        <th>Quantidade</th>
+                        <th>Categoria</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {props.produtos.map((p, i) =>
+                        // <tr key={i} style={i % 2 === 0 ? { backgroundColor: "#ccc" } : { backgroundColor: "#ddd333" }} >
+                        <tr key={i}>
+                            <td>{i + 1}</td>
+                            <td>{p.nomeProduto}</td>
+                            <td>{p.qtd}</td>
+                            <td>{p.categoria}</td>
+                        </tr>
+                    )}
+                </tbody>
+                <tfoot>
+                    <tr>
+                        <td colSpan={4}>
+                            <caption>PRODUTOS</caption>
+                        </td>
+                    </tr>
+                </tfoot>
+            </table>
+        </div >
+    )
+}
