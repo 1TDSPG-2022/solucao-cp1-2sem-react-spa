ef106d3d6dc5eaa646e80038a1f61aad4f0b43ec
ghorita <rm94860@fiap.com.br>
2022-09-09 10:25:47 -0300

    commit

---
+++ rm94860/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function App(props) {
+
+
+
+    function exemplo() {
+        let nome = "Ale"
+
+    }
+
+    console.log(nome)
+
+    return(
+
+    )
+}
