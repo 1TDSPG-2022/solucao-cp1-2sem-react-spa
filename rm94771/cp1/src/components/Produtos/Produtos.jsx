1721296d2c5852160b1aaf5bad21f9ec4c4aa352
GuilhermeRocha0 <gui.rocha009@gmail.com>
2022-09-09 09:34:54 -0300

    Initial commit

---
+++ rm94771/cp1/src/components/Produtos/Produtos.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import './Produtos.css'
+
+export default function Produtos(props) {
+  return (
+    <div>
+      <h2>COMPONENTE PRODUTOS</h2>
+      <table className="tblProdutos">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Produto</th>
+            <th>Quantidade</th>
+            <th>Categoria</th>
+          </tr>
+        </thead>
+        <tbody>
+          {props.listaDeProdutos.map((p, i) => (
+            <tr key={i}>
+              <td>{i + 1}</td>
+              <td>{p.nomeProduto}</td>
+              <td>{p.qtd}</td>
+              <td>{p.categoria}</td>
+            </tr>
+          ))}
+        </tbody>
+        <tfoot>
+          <tr>
+            <td colSpan={4}>
+              <caption>PRODUTOS</caption>
+            </td>
+          </tr>
+        </tfoot>
+      </table>
+    </div>
+  )
+}
